16b5d2d7bf9273ef2bd5b0d1397964d1b19f3278
Teak Costello <TeakC37918@gmail.com>
2014-07-15 01:33:27 -0400

    Added Enemy AI and fixed gravity

--- 2DRogueLike/Assets/EnemyAI.cs
+++ 2DRogueLike/Assets/EnemyAI.cs
@@ -7,10 +7,26 @@ public class EnemyAI : MonoBehaviour {
 	public Sprite[] sprites;
 	public float framesPerSecond;
 
+	public Transform target;
+	public float damping = 1;
+	public float lookAheadFactor = 3;
+	public float lookAheadReturnSpeed = 0.5f;
+	public float lookAheadMoveThreshold = 0.1f;
+	
+	float offsetZ;
+	Vector3 lastTargetPosition;
+	Vector3 currentVelocity;
+	Vector3 lookAheadPos;
+
+
+
+
 	// Use this for initialization
 	void Start () {
 		spriteRenderer = renderer as SpriteRenderer;
-
+		lastTargetPosition = target.position;
+		offsetZ = (transform.position - target.position).z;
+		transform.parent = null;
 
 	}
 	
@@ -19,10 +35,32 @@ public class EnemyAI : MonoBehaviour {
 		int index = (int)(Time.timeSinceLevelLoad * framesPerSecond);
 		index = index % sprites.Length;
 		spriteRenderer.sprite = sprites[index];
+	
+	
+	
 	}
 
 	void FixedUpdate() {
-				transform.rotation = Quaternion.identity;
+
+		transform.rotation = Quaternion.identity;
+
+		float xMoveDelta = (target.position - lastTargetPosition).x;
+		
+		bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
+		
+		if (updateLookAheadTarget) {
+			lookAheadPos = lookAheadFactor * Vector3.right * Mathf.Sign(xMoveDelta);
+		} 
+		else {
+			lookAheadPos = Vector3.MoveTowards(lookAheadPos, Vector3.zero, Time.deltaTime * lookAheadReturnSpeed);	
+		}
+		
+		Vector3 aheadTargetPos = target.position + lookAheadPos + Vector3.forward * offsetZ;
+		Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref currentVelocity, damping);
+		
+		transform.position = newPos;
+		
+		lastTargetPosition = target.position;
 
 		}
 }
---
+++ 2DRogueLike/Assets/Scripts/BetterEnemyAI.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BetterEnemyAI : MonoBehaviour {
+
+	private Vector3 moveDirection;
+	public Transform target;
+	public float moveSpeed;
+	public float turnSpeed;
+
+	public SpriteRenderer spriteRenderer;
+	public Sprite[] sprites;
+	public float framesPerSecond;
+	public float enemyHP = 1;
+	public float currentEnemyHP;
+
+	public float distance = 3;
+	private bool isPatroling;
+	private Vector3 patrol;
+	public Vector2 patrolSpeed = new Vector2 (1.0f, 1.0f);
+
+
+	// Use this for initialization
+	void Start () {
+		moveDirection = Vector3.right;
+		spriteRenderer = renderer as SpriteRenderer;
+
+	}
+	
+
+	void Update () {
+
+		//Set enemy's hp
+		enemyHP = currentEnemyHP;
+
+		if(Vector2.Distance(target.transform.position,this.transform.position) < distance)
+			isPatroling=false;
+		else
+			isPatroling=true;
+
+		if (!isPatroling) {
+						//Set's enemy's current position and target's position
+						target = GameObject.FindWithTag ("Player").transform;
+						Vector3 currentPosition = transform.position;
+						Vector3 moveToward = target.position;
+
+						//Find where to move to move t
+						moveDirection = moveToward - currentPosition;
+						moveDirection.z = 0; 
+						moveDirection.Normalize ();
+
+
+						//Move to target's location
+						Vector3 movetoTarget = moveDirection * moveSpeed + currentPosition;
+						transform.position = Vector3.Lerp (currentPosition, movetoTarget, Time.deltaTime);
+
+						//Rotate sprite towards target's position
+						float targetAngle = Mathf.Atan2 (moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+						transform.rotation = 
+						Quaternion.Slerp (transform.rotation, 
+			                 Quaternion.Euler (0, 0, targetAngle), 
+			                 turnSpeed * Time.deltaTime);
+
+						//Play animation for sprite for moving
+						int index = (int)(Time.timeSinceLevelLoad * framesPerSecond);
+						index = index % sprites.Length;
+						spriteRenderer.sprite = sprites [index];
+				}
+		else if (isPatroling) { 
+			/*
+			patrol = new Vector3(patrolSpeed.x * 3, patrolSpeed.y * 0, 0);
+			Vector3 currentPosition = transform.position;
+			Vector3 moveToward = patrol;
+
+
+			moveDirection = patrol - currentPosition;
+			moveDirection.z = 0; 
+			moveDirection.Normalize ();
+			Vector3 movetoTarget = moveDirection * moveSpeed + currentPosition;
+
+
+			transform.position = Vector3.Lerp (currentPosition, movetoTarget, Time.deltaTime);
+			float targetAngle = Mathf.Atan2 (moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+			transform.rotation = 
+				Quaternion.Slerp (transform.rotation, 
+				                  Quaternion.Euler (0, 0, targetAngle), 
+				                  turnSpeed * Time.deltaTime);
+
+			*/
+			int index = (int)(Time.timeSinceLevelLoad * framesPerSecond);
+			index = index % sprites.Length;
+			spriteRenderer.sprite = sprites [index];
+		
+
+
+
+
+
+		}
+
+		}
+}
+
